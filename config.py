3e8d3fe45fda998fb5db2029bf95c2cb342d7800
lions25262-glitch <lions25262@gmail.com>
2025-12-14 21:00:56 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+# === TOKEN ===
+# BOT_TOKEN = "7529091727:AAEAyp04iMMb4i6W65BozW60LjVcLcRiG9k"
+
+# === Google Sheets config ===
+CREDENTIALS_FILE = "credentials.json"
+SPREADSHEET_NAME = "olimpiada2"
+
+# === Список школ ===
+MAKTABLAR = {
+    "1-maktab": "1",
+    "2-maktab": "2",
+    "3-maktab": "3",
+    "4-maktab": "4"
+}
